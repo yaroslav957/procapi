83d974fb7f14c620b82758972911858c7cb8dca7
yaroslav <yaroslav.lobachev9@gmail.com>
2024-08-06 21:24:34 +0300

    Concept of proc

--- src/process.rs
+++ src/process.rs
@@ -1,12 +1,43 @@
+use std::process::Command;
+use std::str::FromStr;
 
-struct Process {
+pub struct Process {
+    ids: [u32; 2],
     name: String,
-    ids: Ids,
-    cmd: Cmd,
-    state: State,
-    // usage: Usage,
+    cmd: String,
+    state: String,
+    //usage
 }
 
-struct Ids;
-struct Cmd;
-struct State;
+#[cfg(unix)]
+pub fn processes_info() -> Result<Vec<Process>, std::io::Error> {
+    let output = Command::new("ps")
+        .arg("-eo")
+        .arg("pid,ppid,comm,state,command")
+        .output()?;
+    let output_str = String::from_utf8_lossy(&output.stdout);
+    let processes = output_str.lines()
+        .skip(1)
+        .filter_map(|line| {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() >= 5 {
+                let pid = u32::from_str(parts[0]).unwrap_or(0);
+                let ppid = u32::from_str(parts[1]).unwrap_or(0);
+                let name = parts[2].to_string();
+                let state = parts[3].to_string();
+                let cmd = parts[4..].join(" ");
+
+                Some(Process {
+                    name,
+                    ids: [pid, ppid],
+                    cmd,
+                    state,
+                })
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(processes)
+}
