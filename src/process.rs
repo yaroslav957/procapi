55dfa454dc23d0c2d978c1d5aeea22817aa77147
timofey_arkhipov <timar@hotmail.com>
2024-08-07 13:21:52 +0300

    Implement `ProcessState` struct and its parsing from `ps` string

--- src/process.rs
+++ src/process.rs
@@ -2,24 +2,55 @@ use std::process::Command;
 use std::str::FromStr;
 use itertools::Itertools;
 
+#[derive(Debug)]
 pub struct Process {
     ids: [u32; 2],
     name: String,
-    state: String,
+    state: ProcessState,
     cmd: String,
     //usage
 }
 
-impl Process {
-    pub fn new(ids: [u32; 2], name: String, state: String, cmd: String) -> Self {
-        Self {
-            ids,
-            name,
-            cmd,
-            state,
+#[derive(Debug)]
+pub struct ProcessState {
+    run_state: ProcessRunState,
+    // extra
+}
+
+impl ProcessState {
+    pub fn from_str(s: &str) -> Self {
+        ProcessState {
+            run_state: ProcessRunState::from_char(
+                s.chars().nth(0).unwrap_or('Z')
+            ).unwrap()
         }
     }
 }
+
+#[derive(Debug)]
+pub enum ProcessRunState {
+    Idle,
+    Runnable,
+    Sleeping,
+    Stopped,
+    UninterruptibleWait,
+    Dead,
+}
+
+impl ProcessRunState {
+    pub fn from_char(c: char) -> Option<ProcessRunState> {
+        match c {
+            'I' => Some(ProcessRunState::Idle),
+            'R' => Some(ProcessRunState::Runnable),
+            'S' => Some(ProcessRunState::Sleeping),
+            'T' => Some(ProcessRunState::Stopped),
+            'U' => Some(ProcessRunState::UninterruptibleWait),
+            'Z' => Some(ProcessRunState::Dead),
+            _ => None
+        }
+    }
+}
+
 #[cfg(unix)]
 pub fn processes_info() -> Result<Vec<Process>, std::io::Error> {
     let output = Command::new("ps")
@@ -35,7 +66,12 @@ pub fn processes_info() -> Result<Vec<Process>, std::io::Error> {
                 let pid = u32::from_str(pid).unwrap_or(0);
                 let ppid = u32::from_str(ppid).unwrap_or(0);
                 let cmd = parts.join(" ");
-                Some(Process::new([pid, ppid], name.to_string(), state.to_string(), cmd))
+                Some(Process {
+                    ids: [pid, ppid],
+                    name: name.to_string(),
+                    state: ProcessState::from_str(state),
+                    cmd
+                })
             } else { None }
         })
         .collect();
