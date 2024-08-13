f1f4f4278c0c80c93d16f573d9f96613f0bdc19c
timofey_arkhipov <timar@hotmail.com>
2024-08-13 17:18:42 +0300

    fix formatting

--- procapi_core/src/process/platform/macos.rs
+++ procapi_core/src/process/platform/macos.rs
@@ -8,7 +8,10 @@ use libproc::{
     task_info::TaskAllInfo,
     thread_info::ThreadInfo,
 };
-use std::{io::{Error, ErrorKind}, ptr::null_mut};
+use std::{
+    io::{Error, ErrorKind},
+    ptr::null_mut,
+};
 
 use crate::process::{Process, State};
 
@@ -25,41 +28,37 @@ impl TryFrom<i32> for Process {
 
     fn try_from(pid: i32) -> Result<Self, Self::Error> {
         if let Ok(info) = proc_pid::pidinfo::<TaskAllInfo>(pid, 0) {
-            let pth_state = listpidinfo::<ListThreads>(
-                pid,
-                info.ptinfo.pti_threadnum as usize,
-            )
-            .unwrap_or_default()
-            .iter()
-            .filter_map(|&t| pidinfo::<ThreadInfo>(pid, t).ok())
-            .map(|t| match t.pth_run_state {
-                1 => 1, // TH_STATE_RUNNING
-                2 => 5, // TH_STATE_STOPPED
-                3 => {
-                    // TH_STATE_WAITING
-                    if t.pth_sleep_time > 20 {
-                        4
-                    } else {
-                        3
+            let pth_state = listpidinfo::<ListThreads>(pid, info.ptinfo.pti_threadnum as usize)
+                .unwrap_or_default()
+                .iter()
+                .filter_map(|&t| pidinfo::<ThreadInfo>(pid, t).ok())
+                .map(|t| match t.pth_run_state {
+                    1 => 1, // TH_STATE_RUNNING
+                    2 => 5, // TH_STATE_STOPPED
+                    3 => {
+                        // TH_STATE_WAITING
+                        if t.pth_sleep_time > 20 {
+                            4
+                        } else {
+                            3
+                        }
                     }
-                }
-                4 => 2, // TH_STATE_UNINTERRUPTIBLE
-                5 => 6, // TH_STATE_HALTED
-                _ => 7,
-            })
-            .min()
-            .unwrap_or(7);
+                    4 => 2, // TH_STATE_UNINTERRUPTIBLE
+                    5 => 6, // TH_STATE_HALTED
+                    _ => 7,
+                })
+                .min()
+                .unwrap_or(7);
 
             dbg!(info.pbsd);
 
             Ok(Process {
                 ids: [pid as u32, info.pbsd.pbi_ppid],
-                name: proc_pid::name(pid)
-                    .unwrap_or_else(|_| pidpath(pid).unwrap_or_default()),
+                name: proc_pid::name(pid).unwrap_or_else(|_| pidpath(pid).unwrap_or_default()),
                 state: State::try_from(pth_state)?,
-                cmd: get_cmdline(pid as u32).unwrap_or_else(|| {
-					vec![pidpath(pid).unwrap_or_default()]
-				}).join(" "),
+                cmd: get_cmdline(pid as u32)
+                    .unwrap_or_else(|| vec![pidpath(pid).unwrap_or_default()])
+                    .join(" "),
             })
         } else {
             Err(Error::last_os_error())
@@ -92,17 +91,18 @@ fn get_cmdline(pid: u32) -> Option<Vec<String>> {
     let mut mib: [libc::c_int; 3] = [
         libc::CTL_KERN,
         libc::KERN_PROCARGS2,
-        pid.try_into().unwrap()
+        pid.try_into().unwrap(),
     ];
     let mut size: size_t = get_argmax();
-    let process_args: *mut u8 = unsafe {
-        std::mem::transmute(
-            libc::calloc(size, std::mem::size_of::<size_t>())
-        )
-    };
+    let process_args: *mut u8 =
+        unsafe { std::mem::transmute(libc::calloc(size, std::mem::size_of::<size_t>())) };
     let mut res = Vec::<String>::new();
 
-    assert_ne!(process_args, null_mut(), "process args buffer allocation failed");
+    assert_ne!(
+        process_args,
+        null_mut(),
+        "process args buffer allocation failed"
+    );
 
     unsafe {
         if libc::sysctl(
@@ -111,16 +111,15 @@ fn get_cmdline(pid: u32) -> Option<Vec<String>> {
             process_args as *mut c_void,
             &mut size as *mut usize,
             core::ptr::null_mut(),
-            0
-        ) == -1 {
+            0,
+        ) == -1
+        {
             libc::free(process_args as *mut c_void);
             return None;
         }
 
         let mut arg_start: *mut u8 = null_mut();
-        let mut ch_ptr: *mut u8 = process_args.add(
-            std::mem::size_of::<libc::c_int>()
-        );
+        let mut ch_ptr: *mut u8 = process_args.add(std::mem::size_of::<libc::c_int>());
 
         for _ in 0..size {
             if *ch_ptr == b'\0' {
@@ -142,10 +141,7 @@ fn get_cmdline(pid: u32) -> Option<Vec<String>> {
 fn get_argmax() -> size_t {
     let mut sys_max_args = 0i32;
     let mut size = std::mem::size_of::<libc::c_int>();
-    let mut mib: [libc::c_int; 2] = [
-        libc::CTL_KERN,
-        libc::KERN_ARGMAX
-    ];
+    let mut mib: [libc::c_int; 2] = [libc::CTL_KERN, libc::KERN_ARGMAX];
 
     unsafe {
         libc::sysctl(
@@ -154,7 +150,7 @@ fn get_argmax() -> size_t {
             (&mut sys_max_args) as *mut i32 as *mut c_void,
             &mut size,
             null_mut(),
-            0
+            0,
         );
     }
 
@@ -163,7 +159,10 @@ fn get_argmax() -> size_t {
 
 /// Used for extracting data retrieved from kernel, which is guaranteed
 /// to be a valid string
-unsafe fn get_str_unchecked(start: *mut u8, end: *mut u8) -> String {
+unsafe fn get_str_unchecked(
+    start: *mut u8,
+    end: *mut u8,
+) -> String {
     let len = end as usize - start as usize;
     let bytes = Vec::from_raw_parts(start, len, len);
     let s = String::from_utf8_unchecked(bytes.clone());
