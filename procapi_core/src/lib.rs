71ce9fe7636d8436224ecc18533c07260a6e314f
yaroslav <yaroslav.lobachev9@gmail.com>
2024-09-18 21:51:20 +0300

    proj arch

--- procapi_core/src/lib.rs
+++ procapi_core/src/lib.rs
@@ -1,1 +1,27 @@
 pub mod process;
+
+/*
+structure:
+procapi_core (only processes logic)
+    └── process
+        └── platform (Spec. OS impls)
+            └── linux
+                └── init (modules etc)
+                └── ... (modules etc)
+            └── macos
+                └── init (modules etc)
+                └── ... (modules etc)
+            └── other
+                └── init
+                └── ... (modules etc)
+         └── state
+            └── platform (Spec. OS impls)
+                └── linux
+                └── macos
+                └── other
+         └── thread (later)
+            └── platform (Spec. OS impls)
+                └── linux
+                └── macos
+                └── other
+ */
